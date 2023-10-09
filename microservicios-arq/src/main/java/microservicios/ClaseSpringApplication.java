9b4885396b731db05a400f8c1987e5ced694d4cd
noelia2016 <noeliacarrizo22@gmail.com>
2023-10-09 18:22:56 -0300

    Estructura inicial

---
+++ microservicios-arq/src/main/java/microservicios/ClaseSpringApplication.java
@@ -0,0 +1,12 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaseSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaseSpringApplication.class, args);
+	}
+
+}
